02d591b424ea8234725da9181ed84d5763347e2b
Will <WTS012201@gmail.com>
2023-01-28 16:04:32 -0600

    sub

--- server/src/modules/catch/catch.dto.ts
+++ server/src/modules/catch/catch.dto.ts
@@ -3,7 +3,7 @@ import { CatchEntity } from "./catch.entity";
 import { Prediction } from "../prediction/prediction.entity";
 import { Session } from "express-session";
 import { Request } from "express";
-import { IsDate, IsString } from "class-validator";
+import { IsString } from "class-validator";
 
 export class Submission {
   @IsString()
@@ -12,7 +12,7 @@ export class Submission {
   location: string;
   @IsString()
   imageBase64: string;
-  @IsDate()
+  @IsString()
   date: string;
 }
 
