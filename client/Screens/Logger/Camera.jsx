2de3881e8c5a3a5d2a4ff9575db36bed2e8495d6
Will <WTS012201@gmail.com>
2023-02-26 00:16:53 -0600

    upload image to s3

--- client/Screens/Logger/Camera.jsx
+++ client/Screens/Logger/Camera.jsx
@@ -1,14 +1,18 @@
 import { Camera, CameraType } from "expo-camera";
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import useAxios from "axios-hooks";
 import FormData from "form-data";
 import { manipulateAsync, SaveFormat } from "expo-image-manipulator";
-import { API_URL } from "@env";
+import { API_URL, S3_BUCKET } from "@env";
+import { S3 } from "../../utils/connection";
+import { UserContext } from "../../Contexts/UserContext";
+import { Buffer } from "buffer";
 
 export const CameraView = () => {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const { user, setUser } = useContext(UserContext);
   const ref = useRef(null);
 
   const [
@@ -36,6 +40,24 @@ export const CameraView = () => {
     );
   };
 
+  const uploadToS3 = async (base64Image) => {
+    const Bucket = `${S3_BUCKET}/catches`;
+    const params = {
+      Key: `${Date.now()}.${user.username}.jpg`,
+      Body: Buffer.from(
+        base64Image.replace(/^data:image\/\w+;base64,/, ""),
+        "base64"
+      ),
+      ContentType: "jpg",
+      Bucket,
+    };
+
+    const { Location } = await S3.upload(params).promise();
+    console.log("uploading to S3", Location);
+
+    return Location;
+  };
+
   const takeSubmission = async () => {
     const cache = await ref.current.takePictureAsync({
       base64: true,
@@ -52,8 +74,9 @@ export const CameraView = () => {
     ).then((val) => `data:image/jpg;base64,${val.base64}`);
 
     const form = new FormData();
-    form.append("time", "test");
-    form.append("date", "test");
+    const imageUri = await uploadToS3(cache.base64);
+
+    form.append("imageUri", imageUri);
     form.append("imageBase64", resizedImg);
     form.append("location", "test");
     submitCatch({ data: form });
--- client/utils/connection.js
+++ client/utils/connection.js
@@ -1,11 +1,21 @@
 import axios from "axios";
 import { configure } from "axios-hooks";
-import { API_URL } from "@env";
+import { API_URL, S3_ACCESS_KEY, S3_SECRET } from "@env";
+import AWS from "aws-sdk";
 
 export const Client = axios.create({
   baseURL: `http://${API_URL}/`,
   withCredentials: true,
   responseType: "json",
 });
+
+AWS.config.update({
+  accessKeyId: S3_ACCESS_KEY,
+  secretAccessKey: S3_SECRET,
+  region: "us-east-2",
+  signatureVersion: "v4",
+});
+
+export const S3 = new AWS.S3({});
 
 configure({ axios });
