0e50eb451099bebc33fdf0ff7489e9911cf391c2
KPHAN7878 <80300726+KPHAN7878@users.noreply.github.com>
2023-03-09 11:27:21 -0600

    added catch markers to map

--- client/Screens/Map/Map.jsx
+++ client/Screens/Map/Map.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState, useRef, useContext } from "react";
-import { Text, View, StyleSheet } from 'react-native'
-import MapView from 'react-native-maps';
+import { Text, View, StyleSheet, ScrollView, Button, Image } from 'react-native'
+import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
+import { Client } from "../../utils/connection";
 
 const Map = () => {
   const [currentLocation, setLocation] = useState([1, 2]);
   const [errorMsg, setErrorMsg] = useState(null)
+  const [catches, setCatches] = useState([]);
 
   //location services
   React.useEffect(() => {
@@ -30,6 +32,21 @@ const Map = () => {
     text = JSON.stringify(currentLocation);
   }
 
+  const getCatches = async () => {
+    await Client.get("catch")
+    .then((res) => {
+      setCatches(res.data.catches);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  useEffect(() => {
+    getCatches();
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <MapView style={styles.map} 
@@ -40,8 +57,26 @@ const Map = () => {
         //   //longitudeDelta: 0.0421,
         // }}
         showsUserLocation={true}
-      />
-    </View>
+      >
+        {catches.map((item) => {
+          return(<Marker
+            key={item.id}
+            coordinate={{
+              latitude: item.location[0],
+              longitude: item.location[1]
+            }}
+            >
+              <Image 
+                source={require("../../assets/image3.png")}
+                style={{height: 25, width: 23}}
+              />
+            </Marker>
+            )
+        })}
+        <Button title="My Location" style={{position: 'absolute', top: '50%', alignSelf: 'flex-end'}}/>
+      </MapView>
+
+    </View> 
   )
 }
 
