739f8dab2ddf124b3a42e34c92ecd2fc4ec827a0
KPHAN7878 <80300726+KPHAN7878@users.noreply.github.com>
2023-03-08 20:03:09 -0600

    added stack reset after logging out and basic map feature

--- client/Screens/Map/Map.jsx
+++ client/Screens/Map/Map.jsx
@@ -1,12 +1,59 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useState, useRef, useContext } from "react";
+import { Text, View, StyleSheet } from 'react-native'
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
 const Map = () => {
+  const [currentLocation, setLocation] = useState([1, 2]);
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  //location services
+  React.useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location_ = await Location.getCurrentPositionAsync({enableHighAccuracy: false});
+      console.log("maps coordinates: " + location_)
+      setLocation(location_)
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (currentLocation) {
+    text = JSON.stringify(currentLocation);
+  }
+
   return (
-    <View>
-    <Text>MAP</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} 
+        // initialRegion={{
+        //   latitude: currentLocation.coords.latitude,
+        //   longitude: currentLocation.coords.latitude,
+        //   //latitudeDelta: 0.0922,
+        //   //longitudeDelta: 0.0421,
+        // }}
+        showsUserLocation={true}
+      />
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default Map;
